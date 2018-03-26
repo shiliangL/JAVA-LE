2558bb92fafee2d7234849db1880c0ae29598d71
shiliangl <shiliangwei@cloudolp.com>
2018-03-26 10:01:26 +0800

    初始化项目-配置-swagger

--- src/main/java/com/ddesign/controller/Hello.java
+++ src/main/java/com/ddesign/controller/Hello.java
@@ -1,8 +1,6 @@
 package com.ddesign.controller;
 
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
